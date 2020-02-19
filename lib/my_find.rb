d42d775101aab65761e3701d20a392266e439141
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-19 00:24:04 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,12 +1,4 @@
-require 'pry'
-
-#   def my_find(collection)
-#     i = 0
-#   while i < collection.length
-#     return collection[1] if yield(collection[i])
-#       i = i + 1
-#   end
-# end
+reuire 'pry'
 
 def my_find(collection)
   i = 0
